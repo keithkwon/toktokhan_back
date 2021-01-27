dfc61856da0621c48e02d0f051a2af327b361864
Keith Kwon <keithkwon.dev@gmail.com>
2021-01-27 23:41:50 +0900

    fix views register. Better way to process multiple photos

--- toktok_back/cars/views.py
+++ toktok_back/cars/views.py
@@ -17,18 +17,13 @@ from django.contrib.auth import get_user_model
 @permission_classes([IsAuthenticated])
 def register(request):
     if request.method == 'POST':
-        # many=True 추가 예정
-
         serializer = CarSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             car = serializer.save(user = request.user)
-            for image, url in request.data["images"].items() :
-                print(url)
-                photo_serializer = PhotoSerializer(data=url)
-                if photo_serializer.is_valid(raise_exception=True):
-                    photo_serializer.save(car=car)
+            photo_serializer = PhotoSerializer(data=request.data["photos"], many=True)
+            if photo_serializer.is_valid(raise_exception=True):
+                photo_serializer.save(car=car)
             # serializer.save(photos=request.data.images, many=True)
             return Response(serializer.data)
-        print('invalid')
         return Response(serializer.data)
 
